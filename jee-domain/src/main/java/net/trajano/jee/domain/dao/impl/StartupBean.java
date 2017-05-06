92a3cec1e7dea8bf9c85443536052f29ff5601b1
Archimedes Trajano <developer@trajano.net>
2017-05-06 18:32:42 -0400

    Added Liquibase module
    Place holder for now.  May not actually use it since I only have a
    master but should this grow larger this can be used for the delta
    scripts.

--- jee-domain/src/main/java/net/trajano/jee/domain/dao/impl/StartupBean.java
+++ jee-domain/src/main/java/net/trajano/jee/domain/dao/impl/StartupBean.java
@@ -1,8 +1,11 @@
 package net.trajano.jee.domain.dao.impl;
 
+import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.sql.DataSource;
 
 @Singleton
@@ -11,4 +14,16 @@ public class StartupBean {
 
     @Resource(name = "java:app/ds")
     private DataSource ds;
+
+    @PersistenceContext
+    private EntityManager em;
+
+    /**
+     * Ensures that the database works by performing a operation against it.
+     */
+    @PostConstruct
+    public void ensureDatabase() {
+
+        em.createNamedQuery(NamedQueries.PARTICIPANT_GET_ALL).getResultList();
+    }
 }
