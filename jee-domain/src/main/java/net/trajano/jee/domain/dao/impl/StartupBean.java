890b4c56a088d4d3f149d7f17aebda5070e99838
Archimedes Trajano <developer@trajano.net>
2017-05-08 14:54:14 -0400

    Moved java:app/ds to application.xml

--- jee-domain/src/main/java/net/trajano/jee/domain/dao/impl/StartupBean.java
+++ jee-domain/src/main/java/net/trajano/jee/domain/dao/impl/StartupBean.java
@@ -1,19 +1,14 @@
 package net.trajano.jee.domain.dao.impl;
 
 import javax.annotation.PostConstruct;
-import javax.annotation.Resource;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.sql.DataSource;
 
 @Singleton
 @Startup
 public class StartupBean {
-
-    @Resource(name = "java:app/ds")
-    private DataSource ds;
 
     @PersistenceContext
     private EntityManager em;
