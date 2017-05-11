cc45c0b842b1af9b6c6ddf47e8ce4b8d3295767e
Archimedes Trajano <developer@trajano.net>
2017-05-11 10:09:32 -0400

    Added a simple JUnit Kotlin test
    (not working at the moment)
    
    > [ERROR] Failed to execute goal
    org.jetbrains.kotlin:kotlin-maven-plugin:1.1.2-3:test-compile
    (test-compile) on project jee-domain: Compilation failure
    > [ERROR]
    D:\p\jee\jee-domain\src\test\kotlin\net\trajano\jee\domain\kotlin\test\KotlinSanityTest.kt:[7,3]
    Cannot access class 'kotlin.reflect.KClass'. Check your module classpath
    for missing or conflicting dependencies

---
+++ jee-domain/src/test/kotlin/net/trajano/jee/domain/kotlin/test/KotlinSanityTest.kt
@@ -0,0 +1,11 @@
+package net.trajano.jee.domain.kotlin.test
+
+import org.junit.Assert
+import org.junit.Test
+
+class KotlinSanityTest {
+	@Test
+	fun firstTest() {
+		Assert.assertTrue(1 == 1)
+	}
+}
